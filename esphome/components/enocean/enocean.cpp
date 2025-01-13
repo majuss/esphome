eb07d911ae13ed32b4db6e2ad5ec96d6105f0dd7
majuss <biomajuss@gmail.com>
2025-01-13 16:17:41 +0100

    init

--- esphome/components/enocean/enocean.cpp
+++ esphome/components/enocean/enocean.cpp
@@ -1,132 +1,96 @@
-// problem is that mhz19 integration writes values to uart and getting stuff back. we need to listen 24/7
-// a good integration to look at is the gps integration. gps integration does all uart in esphome nothing in library.
-// maybe haier is better
-
 #include "enocean.h"
 #include "esphome/core/log.h"
+#include <enocean.h>
+#include "binary_sensor/enocean_binary_sensor.h"
 
 #define ENOCEAN_HEADER 4
 #define ENOCEAN_MAX_DATA 40
 
-namespace esphome {
-namespace enocean {
+namespace esphome::enocean {
 
 static const char *const TAG = "enocean";
 static const uint8_t ENOCEAN_PACKET_LENGTH = 8;
 
 void Enocean::setup() { ESP_LOGD(TAG, "setup called:"); }
 
-void Enocean::update() {
-  ESP_LOGD(TAG, "update called:");
-
-  if (this->window_handle_ != nullptr)
-    this->window_handle_->publish_state(this->window_state_);
-}
+void Enocean::update() {}
 
 void Enocean::loop() {
-  ESP_LOGD(TAG, "Loop started:");
-  ESP_LOGD(TAG, "foobar:");
-
-  ESP_LOGD(TAG, this->available());
-
   while (this->available()) {
-    // int const b{this->read()};
     uint8_t c;
     this->read_byte(&c);
 
-    // if (b == 0x55) {
-    //   do {
-    //     yield();
-    //   } while (this->available() < ENOCEAN_HEADER + 1);
+    if (c == 0x55) {
+      ESP_LOGD(TAG, "magic byte:");
+      ESP_LOGD(TAG, "%d", enocean_binary_sensor_.size());
+      do {
+        yield();
+      } while (this->available() < ENOCEAN_HEADER + 1);
 
-    //   Packet packet;
+      Packet packet;
 
-    //   this->read_array(packet.getHeader(), ENOCEAN_HEADER);
-    //   byte const CRC8H{this->read()};
+      this->read_array(packet.getHeader(), ENOCEAN_HEADER);
+      int const CRC8H{this->read()};
 
-    //   if (packet.checkHeaderCRC8(CRC8H)) {
-    //     ESP_LOGD(TAG, "Header CRC8 is valid.");
-    //   } else {
-    //     ESP_LOGD(TAG, "Header CRC8 is invalid.");
-    //     return;
-    //   }
-    //   byte waitCounter;
-    //   byte available;
-    //   do {
-    //     yield();
-    //     available = this->available();
+      if (packet.checkHeaderCRC8(CRC8H)) {
+        ESP_LOGD(TAG, "Header CRC8 is valid.");
+      } else {
+        ESP_LOGD(TAG, "Header CRC8 is invalid.");
+        return;
+      }
+      byte waitCounter;
+      byte available;
+      do {
+        yield();
+        available = this->available();
 
-    //   } while (available < packet.payloadLength + 1 || waitCounter++ > 50);
+      } while (available < packet.payloadLength + 1 || waitCounter++ > 50);
 
-    //   if (packet.payloadLength + 1 <= available) {
-    //     Serial.println("read data");
+      if (packet.payloadLength + 1 <= available) {
+        Serial.println("read data");
 
-    //     this->read_array(packet.enocean_data, packet.dataLength);
-    //     this->read_array(packet.enocean_optional, packet.optionalLength);
-    //     byte const CRC8D = this->read();
+        this->read_array(packet.enocean_data, packet.dataLength);
+        this->read_array(packet.enocean_optional, packet.optionalLength);
+        byte const CRC8D = this->read();
 
-    //     if (packet.checkDataCRC8(CRC8D)) {
-    //       ESP_LOGD(TAG, "Data CRC8 is valid.");
-    //     } else {
-    //       ESP_LOGD(TAG, "Data CRC8 is invalid.");
-    //       return;
-    //     };
-    //     packet.handleTelegram();
-    //   }
-    //   if (packet.getState() == "open") {
-    //     this->window_state_ = true;
-    //   } else if (packet.getState() == "closed") {
-    //     this->window_state_ = false;
-    //   }
-    //   ESP_LOGD(TAG, "%s", packet.getState().c_str());
-    //   // for (int i = 0; i < 4; i++)
-    //   // {
-    //   //     Serial.print("senderAddress[");
-    //   //     Serial.print(i);
-    //   //     Serial.print("]: ");
-    //   //     Serial.println(packet.getSenderAddress()[i], HEX);
-    //   // }
+        if (packet.checkDataCRC8(CRC8D)) {
+          ESP_LOGD(TAG, "Data CRC8 is valid.");
+        } else {
+          ESP_LOGD(TAG, "Data CRC8 is invalid.");
+          return;
+        };
+        packet.handleTelegram();
+      }
 
-    // }  // magic byte 0x55
-    // byte const b{this->read()};
+      ESP_LOGD(TAG, "%s", packet.getState().c_str());
 
-    // if (b == 0x55)
-    // {
-    //     do
-    //     {
-    //         yield();
-    //     } while (this->available() < ENOCEAN_HEADER + 1);
+      ESP_LOGD(TAG, "Packet sender: %d %d %d %d", packet.getSenderAddress()[0], packet.getSenderAddress()[1],
+               packet.getSenderAddress()[2], packet.getSenderAddress()[3]);
 
-    //     Packet packet;
-    //
+      for (auto sensor : enocean_binary_sensor_) {
+        if (0 == memcmp(sensor->get_address(), packet.getSenderAddress(), 4)) {
+          if (sensor->get_type() == packet.getType()) {
+            ESP_LOGD(TAG, "Found sensor %s", sensor->get_name());
+            bool const state = packet.getState() == "on";
+            sensor->publish_state(state);
 
-    // Serial.println(packet.state);
-    // for (int i = 0; i < 4; i++)
-    // {
-    //     Serial.print("senderAddress[");
-    //     Serial.print(i);
-    //     Serial.print("]: ");
-    //     Serial.println(packet.senderAddress[i], HEX);
-    // }
-
-  }  // magic byte 0x55
-}  // whileserial available
-}  // namespace enocean
-}  // namespace esphome
-
-// void setup()
-// {
-//     Serial.begin(115200);
-//     Serial.println("Setup starting");
-//     Serial2.begin(57600, SERIAL_8N1, 16, 17);
-
-// }
-
-// void loop()
-// {
-
-//     while (Serial2.available())
-//     {
-
-//     } // while
-// }
+            break;
+          } else if (packet.getType() == "four_button_remote_off") {
+            for (auto sensor : enocean_binary_sensor_) {
+              if (0 == memcmp(sensor->get_address(), packet.getSenderAddress(), 4)) {
+                bool const state = packet.getState() == "on";
+                sensor->publish_state(state);
+              }
+            }
+            break;
+          } else {
+            bool const state = packet.getState() == "on";
+            sensor->publish_state(state);
+            break;
+          }
+        }
+      }
+    }  // magic byte 0x55
+  }  // whileserial available
+}  // function
+}  // namespace esphome::enocean
--- esphome/components/enocean/enocean.h
+++ esphome/components/enocean/enocean.h
@@ -4,12 +4,10 @@
 #include "esphome/core/automation.h"
 #include "esphome/components/binary_sensor/binary_sensor.h"
 #include "esphome/components/uart/uart.h"
-#include <enocean.h>
 
-namespace esphome {
-namespace enocean {
+namespace esphome::enocean {
 
-
+class EnOceanBinarySensor;
 
 class Enocean : public PollingComponent, public uart::UARTDevice {
  public:
@@ -17,56 +15,18 @@ class Enocean : public PollingComponent, public uart::UARTDevice {
 
   void setup() override;
   void update() override;
-  // void dump_config() override;
 
   void loop() override;
 
+  void set_binary_sensor(EnOceanBinarySensor *enocean_binary_sensor) {
+    enocean_binary_sensor_.push_back(enocean_binary_sensor);
+  }
 
-  void set_sensor(binary_sensor::BinarySensor *window_handle) { window_handle_ = window_handle;}
-  
-  // void set_temperature_sensor(sensor::Sensor *temperature_sensor) { temperature_sensor_ = temperature_sensor; }
-  // void set_co2_sensor(sensor::Sensor *co2_sensor) { co2_sensor_ = co2_sensor; }
-  // void set_abc_enabled(bool abc_enabled) { abc_boot_logic_ = abc_enabled ? MHZ19_ABC_ENABLED : MHZ19_ABC_DISABLED; }
+  void set_sensor_id(uint8_t sensor_id) { this->sensor_id_ = sensor_id; }
 
  protected:
-  bool window_state_ = false;
-
-  binary_sensor::BinarySensor *window_handle_{nullptr};
-  Packet packet;
-  // sensor::Sensor *temperature_sensor_{nullptr};
-  // sensor::Sensor *co2_sensor_{nullptr};
-  // MHZ19ABCLogic abc_boot_logic_{MHZ19_ABC_NONE};
+  uint8_t sensor_id_{0};
+  std::vector<EnOceanBinarySensor *> enocean_binary_sensor_{};
 };
 
-// template<typename... Ts> class MHZ19CalibrateZeroAction : public Action<Ts...> {
-//  public:
-//   MHZ19CalibrateZeroAction(MHZ19Component *mhz19) : mhz19_(mhz19) {}
-
-//   void play(Ts... x) override { this->mhz19_->calibrate_zero(); }
-
-//  protected:
-//   MHZ19Component *mhz19_;
-// };
-
-// template<typename... Ts> class MHZ19ABCEnableAction : public Action<Ts...> {
-//  public:
-//   MHZ19ABCEnableAction(MHZ19Component *mhz19) : mhz19_(mhz19) {}
-
-//   void play(Ts... x) override { this->mhz19_->abc_enable(); }
-
-//  protected:
-//   MHZ19Component *mhz19_;
-// };
-
-// template<typename... Ts> class MHZ19ABCDisableAction : public Action<Ts...> {
-//  public:
-//   MHZ19ABCDisableAction(MHZ19Component *mhz19) : mhz19_(mhz19) {}
-
-//   void play(Ts... x) override { this->mhz19_->abc_disable(); }
-
-//  protected:
-//   MHZ19Component *mhz19_;
-// };
-
-}  // namespace mhz19
-}  // namespace esphome
+}  // namespace esphome::enocean
